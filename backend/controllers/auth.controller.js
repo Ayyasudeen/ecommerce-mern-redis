86bf924018e11449f5c0bacffaa822d66b434610
unknown <ayyasudeen@gmail.com>
2024-12-03 18:02:54 +0530

    controller

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,9 @@
+export const signup = async (req, res) => {
+    res.send("Sign Up Route Called")
+}
+export const login = async (req, res) => {
+    res.send("login Route Called")
+}
+export const logout = async (req, res) => {
+    res.send("logout Route Called")
+}
--- notes.md
+++ notes.md
@@ -1,3 +1,4 @@
 1. We have common package json because it will be useful for deployment process.
 npm i express dotenv mongoose jsonwebtoken stripe cloudinary cookie-parser bcryptjs ioredis
 2. http://localhost:5000/api/auth/signup
+14:25
