bbce96519e38b446c88a7c60b9c279ed2c770264
unknown <ayyasudeen@gmail.com>
2024-12-03 17:58:17 +0530

    routes

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -5,5 +5,11 @@ const router = express.Router();
 router.get("/signup", (req, res)=> {
     res.send("Sign up route called")
 })
+router.get("/login", (req, res)=> {
+    res.send("login route called")
+})
+router.get("/logout", (req, res)=> {
+    res.send("logout route called")
+})
 
 export default router
